0da4dd80b4a3a44229083181111298d364fb59d0
George Okello <okellogeorge40@yahoo.com>
2018-02-03 23:46:59 -0600

    Add db context extensions

---
+++ SimImageGallery/SimImageGalleryDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace SimImageGallery.Data
+{
+    public class SimImageGalleryDbContext : DbContext
+    {
+        public SimImageGalleryDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
