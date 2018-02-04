f09e30a4cdc06b532a77f48f48cbf622a4aa646b
George Okello <okellogeorge40@yahoo.com>
2018-02-03 23:41:31 -0600

    Add project files.

---
+++ ImageGallery/Controllers/ImGalleryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageGallery.Controllers
+{
+    public class ImGalleryController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new 
+            return View();
+        }
+    }
+}
