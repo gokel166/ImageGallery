e16a5e2041ba8d096a3a7dae3dc4bbbbf5e8f25c
George Okello <okellogeorge40@yahoo.com>
2018-02-04 23:18:02 -0600

    complete image gallery

--- ImageGallery/Controllers/ImGalleryController.cs
+++ ImageGallery/Controllers/ImGalleryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ImageGallery.Models;
+using SimImageGallery.Models;
 
 namespace ImageGallery.Controllers
 {
@@ -11,11 +12,63 @@ namespace ImageGallery.Controllers
     {
         public IActionResult Index()
         {
+            var hikingImageTags = new List<ImageTag>();
+            var cityImageTags = new List<ImageTag>();
+
+            var tag1 = new ImageTag()
+            {
+                Description = "Adventure",
+                Id = 0
+            };
+
+            var tag2 = new ImageTag()
+            {
+                Description = "Urban",
+                Id = 1
+            };
+
+            var tag3 = new ImageTag()
+            {
+                Description = "New York",
+                Id = 2
+            };
+
+            hikingImageTags.Add(tag1);
+            cityImageTags.AddRange(new List<ImageTag> { tag2, tag3 });
+
+            var imageList = new List<GalleryImage>()
+            {
+                new GalleryImage()
+                {
+                    Title = "Hiking Trip",
+                    Url = "https://backroads-web.s3.amazonaws.com/images/search/thumbnail/french-italian-alps-walking-hiking-tour.jpg",
+                    Created = DateTime.Now,
+                    Tags = hikingImageTags
+                },
+
+                new GalleryImage()
+                {
+                    Title = "On the Trail",
+                    Url = "https://gunnewsdaily.com/wp-content/uploads/2017/06/trail.jpg",
+                    Created = DateTime.Now,
+                    Tags = hikingImageTags
+                },
+
+                new GalleryImage()
+                {
+                    Title = "Downtown",
+                    Url = "https://www.omnihotels.com/-/media/images/hotels/pvddtn/activities/pvddtn-omni-providence-cityscape.jpg?h=660&la=en&w=1170",
+                    Created = DateTime.Now,
+                    Tags = cityImageTags
+                }
+            };
+
             var model = new GalleryIndexModel()
             {
-                //
+                Images = imageList,
+                SearchQuery = ""
             };
-            return View();
+            return View(model);
         }
     }
 }
