78fe6354116cb4a8b1e1388f0839acdf2d89ca42
George Okello <okellogeorge40@yahoo.com>
2018-02-05 00:15:46 -0600

    Finish Image service and interface

---
+++ SimImageGallery/IImage.cs
@@ -0,0 +1,14 @@
+﻿using SimImageGallery.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimImageGallery.Data
+{
+    public interface IImage
+    {
+        IEnumerable<GalleryImage> GetAll();
+        IEnumerable<GalleryImage> GetWithTag(string tag);
+        GalleryImage GetById(int id);
+    }
+}
--- SimpleImageGallery.Services/ImageService.cs
+++ SimpleImageGallery.Services/ImageService.cs
@@ -1,8 +1,34 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using SimImageGallery.Data;
+using SimImageGallery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SimpleImageGallery.Services
 {
-    public class ImageService
+    public class ImageService : IImage
     {
+        private readonly SimImageGalleryDbContext _ctx;
+
+        public ImageService(SimImageGalleryDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IEnumerable<GalleryImage> GetAll()
+        {
+            return _ctx.GalleryImages.Include(img => img.Tags);
+        }
+                
+        public IEnumerable<GalleryImage> GetWithTag(string tag)
+        {
+            return GetAll().Where(img => img.Tags.Any(t => t.Description == tag));
+        }
+
+        public GalleryImage GetById(int id)
+        {
+            return _ctx.GalleryImages.Find(id);
+        }
     }
 }
